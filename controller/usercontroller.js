94c60ce1e6ecd5ffbf125b684f766048a02de55c
Aswin-Annalath1 <aswinannalath2001@gmail.com>
2024-04-24 13:13:38 +0530

    changes

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,6 +1,7 @@
 // Here we write logic to be done..
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")     //Creating unique web token 
+const nodemailer = require('nodemailer')
 
 //Function to generate token created
 const createToken = (id => {             //id taken from user data..
@@ -42,3 +43,33 @@ module.exports.logout = (req,res) => {
     res.cookie("jwt", "", {maxAge: 1}) 
     res.json({message: "Logged out"})
 }
+
+//Here we are verifying
+module.exports.send_verification_email = (req, res) => {
+    const { userId, email } = req.body;
+  
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'aswinannalath2001@gmail.com', 
+        pass: 'ygcy birh rllq tvgv', 
+      },
+    });
+  
+    const mailOptions = {
+      from: 'aswinannalath2001@gmail.com',
+      to: email,
+      subject: 'Verify Your Email',
+      html: `<p>This mail is just to confirm that you have registered in our website</p>`,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        res.json({ success: false });
+      } else {
+        console.log('Email sent:', info.response);
+        res.json({ success: true });
+      }
+    });
+  }
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,10 +1,11 @@
 
-const {register,login,logout} = require("../controller/usercontroller")
+const {register,login,logout,send_verification_email} = require("../controller/usercontroller")
 
 const router = require('express').Router();
 
 router.post("/register", register)
 router.post("/login", login)
 router.get("/logout", logout)
+router.post('/send-verification-email',send_verification_email)
 
 module.exports = router
